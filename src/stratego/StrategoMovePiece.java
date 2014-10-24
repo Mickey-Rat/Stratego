e0fa3fcb358aa41d394c480784c5ea5474db299e
Kyle Clark <kyky131@gmail.com>
2014-10-24 12:49:16 -0700

    Code
    HERE IS CODE BRIAN

---
+++ src/stratego/StrategoMovePiece.java
@@ -0,0 +1,7 @@
+/*Authors: Kyle Clark, Andrew Gladkowski, Brian Sangster*/
+package stratego;
+import java.awt.*;
+/*This class will be responsible for moving the piece(Animate), showing possible moves, and holding the atributes of each piece.*/
+public class StrategoMovePiece {
+    
+}
