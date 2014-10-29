1d3e7e45f96578018d60ae3fa3d22095d8cb3db5
Kyle Clark <kyky131@gmail.com>
2014-10-29 12:18:45 -0700

    Revert "MORE CODE"
    This reverts commit 31c2a5d7fd43cdff50ff34865f4dd9faf411e3ad.

--- src/stratego/StrategoMovePiece.java
+++
@@ -1,7 +0,0 @@
-/*Authors: Kyle Clark, Andrew Gladkowski, Brian Sangster*/
-package stratego;
-import java.awt.*;
-/*This class will be responsible for moving the piece(Animate), showing possible moves, and holding the atributes of each piece.*/
-public class StrategoMovePiece {
-    
-}
